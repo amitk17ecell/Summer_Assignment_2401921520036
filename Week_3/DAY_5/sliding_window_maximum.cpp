4f5472a0db53a762739b091c2bb9648b96de3409
amitk17ecell <amit171105@gmail.com>
2026-06-21 10:18:26 +0530

    Add Week_3 Day_5 solutions: queue using stack, recent calls, sliding window maximum

---
+++ Week_3/DAY_5/sliding_window_maximum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution 
+{
+  public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq; // stores indices
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if (i >= k - 1) 
+            {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+        return result  ;
+    }
+};
+int main() 
+{
+    Solution sol ;
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3 ;
+    vector<int> ans = sol.maxSlidingWindow(nums, k);
+    cout << "Sliding Window Maximums: ";
+    for (int x : ans) 
+    {
+        cout << x << " ";
+    }
+    cout << endl ;
+    return 0 ;
+}
